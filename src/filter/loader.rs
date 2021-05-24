7b80969dae25dfa9848df3e7d81adcd5c8dc081d
n1k0r <git@n1k0r.me>
2021-05-24 21:21:27 +0300

    add loader thread

---
+++ src/filter/loader.rs
@@ -0,0 +1,212 @@
+use super::storage::{FilterID, FilterStorage};
+
+use bpf::{Link, Object};
+use libbpf_rs as bpf;
+use nix::{self, sys::socket::SockAddr};
+use serde::{Deserialize, Serialize};
+
+use std::{collections::HashMap, path::Path, sync::mpsc::{self, Sender, Receiver}};
+
+#[allow(dead_code)]
+struct LoadedFilter {
+    object: Object,
+    link: Link,
+}
+
+type LoadRequest = FilterID;
+
+type LoadResponse = Result<(), LoadError>;
+
+pub struct Loader {
+    sender: Sender<LoadRequest>,
+    receiver: Receiver<LoadResponse>,
+}
+
+struct LoaderThread {
+    sender: Sender<LoadResponse>,
+    receiver: Receiver<LoadRequest>,
+    filters: HashMap<String, LoadedFilter>,
+    debug: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum LoadError {
+    StorageNotExist(FilterID),
+    InvalidStorage(FilterID),
+    MarkStorage(FilterID, String),
+    IfacesList,
+    IfaceNotExist(String),
+    Open(String),
+    Load(String),
+    Attach(String),
+    InternalError,
+}
+
+impl Loader {
+    pub fn new() -> Self {
+        let (req_tx, req_rx) = mpsc::channel();
+        let (resp_tx, resp_rx) = mpsc::channel();
+
+        std::thread::spawn(|| {
+            let mut loader = LoaderThread::new(req_rx, resp_tx);
+            loader.work();
+        });
+
+        Loader {
+            sender: req_tx,
+            receiver: resp_rx,
+        }
+    }
+
+    pub fn load(&mut self, id: FilterID) -> Result<(), LoadError> {
+        if let Err(_) = self.sender.send(id.clone()) {
+            return Err(LoadError::InternalError);
+        }
+
+        let resp = match self.receiver.recv() {
+            Ok(resp) => resp,
+            Err(_) => return Err(LoadError::InternalError),
+        };
+
+        resp
+    }
+}
+
+impl LoaderThread {
+    pub fn new(rx: Receiver<LoadRequest>, tx: Sender<LoadResponse>) -> Self {
+        let mut loader = Self {
+            receiver: rx,
+            sender: tx,
+            filters: HashMap::new(),
+            debug: false,
+        };
+
+        if let Some(storage) = FilterStorage::load_current() {
+            let _ = loader.load(&storage);
+        }
+
+        loader
+    }
+
+    pub fn work(&mut self) {
+        while let Ok(req) = self.receiver.recv() {
+            let result = self.request(req);
+            self.sender.send(result).unwrap();
+        }
+    }
+
+    fn request(&mut self, req: LoadRequest) -> LoadResponse {
+        let storage = match FilterStorage::load(req) {
+            Some(storage) => storage,
+            None => return Err(LoadError::StorageNotExist(req)),
+        };
+
+        if let Err(err) = self.load(&storage) {
+            return Err(err);
+        }
+
+        Ok(())
+    }
+
+    fn load(&mut self, storage: &FilterStorage) -> Result<(), LoadError> {
+        if let Err(err) = storage.mark_current() {
+            return Err(LoadError::MarkStorage(storage.id(), err.to_string()));
+        }
+
+        let default = match storage.get_object(None) {
+            Some(path) => path,
+            None => return Err(LoadError::InvalidStorage(storage.id())),
+        };
+
+        let ifaces = match get_ifaces() {
+            Some(ifaces) => ifaces,
+            None => return Err(LoadError::IfacesList),
+        };
+
+        for iface in ifaces.iter() {
+            let path = storage.get_object(Some(iface));
+            let path = path.as_ref().unwrap_or(&default);
+            self.load_iface(&path, iface)?;
+        }
+
+        Ok(())
+    }
+
+    fn load_iface(&mut self, path: &Path, iface: &str) -> Result<(), LoadError> {
+        let ifindex = match get_ifindex(iface) {
+            Some(index) => index,
+            None => return Err(LoadError::IfaceNotExist(iface.to_string())),
+        };
+
+        let mut builder = bpf::ObjectBuilder::default();
+        builder.debug(self.debug);
+
+        let mut openobj = match builder.open_file(path) {
+            Ok(openobj) => openobj,
+            Err(error) => return Err(
+                LoadError::Open(format!("{}", error))
+            ),
+        };
+        let oprog = openobj.prog("main").unwrap().unwrap();
+        oprog.set_prog_type(bpf::ProgramType::Xdp);
+
+        let mut obj = match openobj.load() {
+            Ok(obj) => obj,
+            Err(error) => return Err(
+                LoadError::Load(format!("{}", error))
+            ),
+        };
+
+        let prog = obj.prog_unwrap("main");
+
+        self.filters.remove(iface);
+
+        let link = match prog.attach_xdp(ifindex as i32) {
+            Ok(link) => link,
+            Err(error) => return Err(
+                LoadError::Attach(format!("{}", error))
+            ),
+        };
+
+        self.filters.insert(iface.to_string(), LoadedFilter {
+            object: obj,
+            link: link,
+        });
+
+        Ok(())
+    }
+}
+
+fn get_ifaces() -> Option<Vec<String>> {
+    let mut ifaces = vec![];
+
+    let iface_it = match nix::ifaddrs::getifaddrs() {
+        Ok(it) => it,
+        Err(_) => return None,
+    };
+
+    for iface in iface_it {
+        if let Some(SockAddr::Link(_)) = iface.address {
+            ifaces.push(iface.interface_name);
+        }
+    }
+
+    Some(ifaces)
+}
+
+fn get_ifindex(iface_name: &str) -> Option<usize> {
+    let iface_it = match nix::ifaddrs::getifaddrs() {
+        Ok(it) => it,
+        Err(_) => return None,
+    };
+
+    for iface in iface_it {
+        if iface.interface_name == iface_name {
+            if let Some(SockAddr::Link(addr)) = iface.address {
+                return Some(addr.ifindex());
+            }
+        }
+    }
+
+    None
+}
---
+++ src/filter/storage.rs
@@ -0,0 +1,133 @@
+use rand::Rng;
+
+use std::{io::Error as IOError, path::PathBuf};
+
+const STORAGE_PATH: &str = "/var/lib/xnf/filters";
+const CURRENT_PATH: &str = "/var/lib/xnf/current";
+
+pub type FilterID = u64;
+
+pub struct FilterStorage {
+    id: FilterID,
+}
+
+impl FilterStorage {
+    pub fn new() -> Result<Self, IOError> {
+        let mut rng = rand::thread_rng();
+        let mut id;
+
+        loop {
+            id = rng.gen();
+            if Self::load(id).is_none() {
+                break;
+            }
+        }
+
+        let path = filter_path(id);
+        std::fs::create_dir_all(path)?;
+
+        let storage = Self::load(id).unwrap();
+        Ok(storage)
+    }
+
+    pub fn load(id: FilterID) -> Option<Self> {
+        let path = filter_path(id);
+        if !path.is_dir() {
+            return None;
+        }
+
+        let storage = Self {
+            id,
+        };
+        Some(storage)
+    }
+
+    pub fn load_current() -> Option<Self> {
+        let path = PathBuf::from(CURRENT_PATH);
+        let real_path = match std::fs::read_link(&path) {
+            Ok(path) => path,
+            Err(_) => return None,
+        };
+
+        let expected_path = PathBuf::from(STORAGE_PATH);
+
+        match real_path.parent() {
+            Some(parent) => {
+                if parent != expected_path {
+                    return None;
+                }
+            },
+            None => return None,
+        }
+
+        let name = real_path.file_name().unwrap().to_str().unwrap();
+        let id: FilterID = match FilterID::from_str_radix(name, 16) {
+            Ok(id) => id,
+            Err(_) => return None,
+        };
+
+        let storage = Self {
+            id,
+        };
+
+        Some(storage)
+    }
+
+    pub fn id(&self) -> FilterID {
+        self.id
+    }
+
+    pub fn get_object(&self, iface: Option<&str>) -> Option<PathBuf> {
+        let path = self.build_object_path(iface);
+
+        if !path.is_file() {
+            return None;
+        }
+
+        Some(path)
+    }
+
+    pub fn save_object(&self, iface: Option<&str>) -> Option<PathBuf> {
+        let path = self.build_object_path(iface);
+
+        if path.is_file() {
+            return None;
+        }
+
+        Some(path)
+    }
+
+    pub fn mark_current(&self) -> Result<(), IOError> {
+        let filter_path = filter_path(self.id);
+        let current_link = PathBuf::from(CURRENT_PATH);
+        if current_link.exists() {
+            std::fs::remove_file(&current_link)?;
+        }
+
+        std::os::unix::fs::symlink(&filter_path, &current_link)?;
+
+        Ok(())
+    }
+
+    fn build_object_path(&self, iface: Option<&str>) -> PathBuf {
+        let mut path = filter_path(self.id);
+
+        if let Some(iface) = iface {
+            let name = format!("iface_{}.o", iface);
+            path.push(name);
+        } else {
+            path.push("iface.so");
+        }
+
+        path
+    }
+}
+
+fn filter_path(id: FilterID) -> PathBuf {
+    let mut path = PathBuf::from(STORAGE_PATH);
+
+    let hex = format!("{:016x}", id);
+    path.push(hex);
+
+    path
+}
